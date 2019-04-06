0a296f2553b1a8d1a3d5e171e436bbd42dd7437c
Kolbe Weathington <kolbe.weathington>
2019-04-05 22:27:44 -0500

    Green View

--- src/Views/MusicViz.kt
+++ src/Views/MusicViz.kt
@@ -2,6 +2,10 @@ package Views
 
 import GraphicsPlayground.Views.GraphicsView
 import javafx.scene.layout.Pane
+import javafx.scene.layout.StackPane
+import javafx.scene.media.Media
+import javafx.scene.media.MediaPlayer
+import javafx.scene.media.MediaView
 import net.beadsproject.beads.analysis.Analyzer
 import net.beadsproject.beads.analysis.FeatureExtractor
 import net.beadsproject.beads.analysis.FeatureManager
@@ -13,19 +17,29 @@ import net.beadsproject.beads.data.Sample
 import net.beadsproject.beads.data.SampleManager
 import net.beadsproject.beads.ugens.Gain
 import net.beadsproject.beads.ugens.SamplePlayer
+import java.io.File
+import java.util.*
 
-class MusicViz:GraphicsView {
+class MusicViz : GraphicsView {
 
 
     override val label: String = "Music Visualizer"
-    override var root: Pane = Pane()
+    override var root: Pane = StackPane()
+
+    lateinit var media: Media
+    lateinit var player: MediaPlayer
+    lateinit var mediaView: MediaView
+
+    init {
+        root.styleClass.add("MusicViz")
+    }
 
     override fun onOpen() {
 
     }
 
     override fun onClose() {
-        
+
     }
 
     override fun willOpen() {
@@ -33,31 +47,11 @@ class MusicViz:GraphicsView {
     }
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//    lateinit var player : SamplePlayer
-////    lateinit var sample : Sample
-////    var context = AudioContext()
-////
-////    lateinit var analyzer: Analyzer
-////    var fft: FFT = FFT()
-////    val beads = BeadArray()
-////
-////
-////
-////    private fun setup(){
-////
-////        sample = Sample("/Users/kolbe/IdeaProjects/GraphicsPlayground/src/Views/Copy of Liberty City House (Updatedd).wav")
-////        player = SamplePlayer(context,sample)
-////        val g = Gain(context, 2, 0.2f)
-////        g.addInput(player)
-////        context.out.addInput(g)
-////        context.start()
-////
-////        println("Number Frames: ${sample.numFrames}")
-////        println("Number Channels: ${sample.numChannels}")
-////
-////    }
-////
-////    inner class analSettings:FFT(){
-
+    fun setup() {
+        media = Media("Views/Copy of Liberty City House (Updatedd).wav")
+        player = MediaPlayer(media)
+        mediaView = MediaView(player)
+        root.children.add(mediaView)
     }
+
 }
